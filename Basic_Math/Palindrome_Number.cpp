f5d3fe6d42ae33085dcc6f67f137a472e83cf0e6
Fardin Chowdhury <cfardin51@gmail.com>
2025-11-27 19:01:16 +0600

    Create Palindrome_Number.cpp

---
+++ Basic_Math/Palindrome_Number.cpp
@@ -0,0 +1,17 @@
+// Palindrome Number (leetcode easy)
+// https://leetcode.com/problems/palindrome-number/description/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int n = s.size();
+        for(int i=0; i<n; i++){
+            if(s[i] != s[n-1-i]){
+                return false;
+                break;
+            }
+        }
+        return true;
+    }
+};
